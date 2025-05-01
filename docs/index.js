f52d6c7206473b7b5664cef59bd0cc3413507e55
darkoverlordofdata <darkoverlordofdata@gmail.com>
2025-05-01 13:42:01 -0700

    update

--- docs/index.js
+++ docs/index.js
@@ -31,7 +31,7 @@ if (ENVIRONMENT_IS_NODE) {}
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpre1gomjm.js
+// include: /tmp/tmpq5zlxf6k.js
 Module["expectedDataFileDownloads"] ??= 0;
 
 Module["expectedDataFileDownloads"]++;
@@ -332,7 +332,7 @@ Module["expectedDataFileDownloads"]++;
   });
 })();
 
-// end include: /tmp/tmpre1gomjm.js
+// end include: /tmp/tmpq5zlxf6k.js
 // Sometimes an existing Module object exists with properties
 // meant to overwrite the default module functionality. Here
 // we collect those properties and reapply _after_ we configure
